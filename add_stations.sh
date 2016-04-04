a25b4858087ae1ee260df5248f217db79b5e29df
Alex Ellis <alexellis2@gmail.com>
2016-04-04 21:19:37 +0100

    Add echo

--- add_stations.sh
+++ add_stations.sh
@@ -7,25 +7,27 @@ fi
 rm ~/.mpd/playlists/*
 
 mpc clear
-curl http://www.radiofeeds.co.uk/bbcradio1.pls|cat|grep 'File1'|cut -d\= -f2-20 |mpc add
+curl -s http://www.radiofeeds.co.uk/bbcradio1.pls|cat|grep 'File1'|cut -d\= -f2-20 |mpc add
 mpc save BBCRadio1
 
 mpc clear
-curl http://www.radiofeeds.co.uk/bbcradio2.pls|cat|grep 'File1'|cut -d\= -f2-20 |mpc add
+curl -s http://www.radiofeeds.co.uk/bbcradio2.pls|cat|grep 'File1'|cut -d\= -f2-20 |mpc add
 mpc save BBCRadio2
 
 mpc clear
-curl http://www.radiofeeds.co.uk/bbcradio4fm.pls|cat|grep 'File1'|cut -d\= -f2-20 |mpc add
+curl -s http://www.radiofeeds.co.uk/bbcradio4fm.pls|cat|grep 'File1'|cut -d\= -f2-20 |mpc add
 mpc save BBCRadio4
 
 mpc clear
-curl http://www.radiofeeds.co.uk/bbc6music.pls|cat|grep 'File1'|cut -d\= -f2-20 |mpc add
+curl -s http://www.radiofeeds.co.uk/bbc6music.pls|cat|grep 'File1'|cut -d\= -f2-20 |mpc add
 mpc save BBC6Music
 
 mpc clear
-curl http://icy-e-ba-08-boh.sharp-stream.com/kissnational.mp3.m3u|cat|mpc add
+curl -s http://icy-e-ba-08-boh.sharp-stream.com/kissnational.mp3.m3u|cat|mpc add
 mpc save KissFM
 
 mpc clear
-curl http://media-ice.musicradio.com/CapitalXTRANationalMP3.m3u|cat|mpc add
+curl -s http://media-ice.musicradio.com/CapitalXTRANationalMP3.m3u|cat|mpc add
 mpc save CapitalXtra
+
+echo "Stations added."
